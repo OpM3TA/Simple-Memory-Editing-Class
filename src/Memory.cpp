d9be94c9a3f4d64f8935a6598fbb278ef304fdce
OpM3TA <xmodfree@gmail.com>
2015-06-09 11:51:34 -0400

    Update Memory.cpp

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -36,7 +36,7 @@ void Memory::Write(LPCVOID Address, int Value)
 
 	}
 }
-int Memory::Read(LPCVOID Address)
+DWORD Memory::Read(LPCVOID Address)
 {
 	int value = 0;
 	HANDLE phandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PROCESSID);
@@ -47,15 +47,66 @@ int Memory::Read(LPCVOID Address)
 		}
 	}
 }
-// destroy data and such; later.
+
+
+DWORD Memory::FindPointer(int level, int baseaddr, int offsets[])
+{
+	DWORD Address = baseaddr;
+	int total = level;
+	HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PROCESSID);
+	for (int i = 0; i < total; i++) //Loop trough the offsets
+	{
+		ReadProcessMemory(pHandle, (LPCVOID)Address, &Address, 4, NULL);
+		Address += offsets[i];
+	}
+	return Address;
+}
+DWORD Memory::getModuleBaseAddress(TCHAR * lpszModuleName, DWORD initOffset)
+{
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, PROCESSID);
+	DWORD moduleBaseAddr = 0;
+
+	if (hSnapshot != INVALID_HANDLE_VALUE)
+	{
+		MODULEENTRY32 mentry32 = { 0 };
+		mentry32.dwSize = sizeof(MODULEENTRY32);
+
+		if (Module32First(hSnapshot, &mentry32))
+		{
+			do
+			{
+				if (_tcscmp(mentry32.szModule, lpszModuleName) == 0)
+				{
+					moduleBaseAddr = (DWORD)mentry32.modBaseAddr;
+					break;
+				}
+			} while (Module32Next(hSnapshot, &mentry32));
+		}
+	}
+	else
+	{
+		std::cout << "Error on finding module base address: " << GetLastError() << "\n";
+	}
+	BASE_ADDRESS = moduleBaseAddr + initOffset;
+	return moduleBaseAddr+initOffset;
+}
+
+
+
+
+
+
+
+
+
+// destroy data and such
 Memory::~Memory()
 {
 }
 
 
-
 /*
-Old stuff from an interesting idea :p
+
 void activate_switch(int addr, DWORD processid, uint16_t newValue = NOP){
 write_addr((LPCVOID)addr, newValue, processid);
 }
@@ -66,6 +117,8 @@ return read_addr((LPCVOID)addr, processid);
 
 
 //  TODO: Add error handling/returning to check if successful
+
+
 
 
 
