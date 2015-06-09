f2651afe13d60907adbdb3a596a8ddc80495f61e
OpM3TA <xmodfree@gmail.com>
2015-06-09 11:51:51 -0400

    Update Memory.h

--- src/Memory.h
+++ src/Memory.h
@@ -3,18 +3,21 @@
 #include <fstream>
 #include <Windows.h>
 #include <cstdint>
-
-
+#include <tchar.h>
+#include <TlHelp32.h>
+#include <tchar.h>
 class Memory
 {
 public:
 	DWORD PROCESSID = 0;
-	//HANDLE PROCESS = 0;
-	
+	HANDLE PROCESS = 0;
+	DWORD BASE_ADDRESS = 0;
 	Memory(DWORD pid=0);
 	void Write(LPCVOID Address, int Value);
-	int Read(LPCVOID Address);
+	DWORD Read(LPCVOID Address);
 
+	DWORD getModuleBaseAddress(TCHAR * lpszModuleName, DWORD initOffset=0);
+	DWORD FindPointer(int level, int baseaddr, int offsets[]);
 	void GetProcessFromWindow(const char*);
 	~Memory();
 };
