542c8358f18e5834b889f95ebc401230b9f0b0e1
OpM3TA <xmodfree@gmail.com>
2015-06-09 11:50:31 -0400

    Update Example.cpp

--- src/Example.cpp
+++ src/Example.cpp
@@ -1,9 +1,8 @@
-#include "Memory.h"
-
 #include "stdafx.h"
 #include <Windows.h>
 #include <iostream>
 #include <string>
+
 
 #include "Memory.h"
 
@@ -11,20 +10,21 @@ using namespace std;
 char *menu = "1. Toggle GodMode\n"
 			 "2. NoClip\n"
 			 "3. Reset Character\n";
-			 
-// We are NOT in the processes address space, unless we make a dll, so don't forget to openproc & VirtualProtect
-
-#define NOP 0x90 // I think thats right
 
 // Switches
-// Fill in for fun :P
+
+
+#define NOP 0x90 // I think thats right
+// We are NOT in the processes address space, unless we make a dll, so don't forget to openproc & VirtualProtect
 struct addr_switches {
 
-//	int GodMode = 0x000000;
+	int GodMode = 0x000000;
 	int NoClip = 0x000000;
 	int Reset = 0x000000;
 	int Health = 0x00248C58;
 } addresses;
+
+
 
 
 
@@ -34,11 +34,12 @@ int main(int argc, _TCHAR* argv[])
 	const char *title = "Step 2";
 	Memory *roblox = new Memory();
 	roblox->GetProcessFromWindow(title);
-	// Later on perhaps do what I did before. if no value is supplied auto-Nop the address
-	// roblox->Write(addresses.Health, NOP); 
-	roblox->Write(addresses.Health, 150); 
+	roblox->getModuleBaseAddress(_TEXT("Tutorial-i386.exe"), 0x00245300);
 
-	
+	int offsets[] = { 0x754, 0x24c, 0x104, 0x8, 0x6c0 };
+	DWORD addr = roblox->FindPointer(5, roblox->BASE_ADDRESS, offsets);
+
+
 	cout << menu;
 	int cmd;
 	while (true) {
@@ -48,7 +49,10 @@ int main(int argc, _TCHAR* argv[])
 		switch (cmd) {
 		case 1:
 			cout << "Activating GodMode\n";
-
+			roblox->Write((LPCVOID)addr, 1000);
+			break;
+		default:
+			cout << cmd <<endl;
 			break;
 		}
 
